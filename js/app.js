7c61814baa7e4e2e9b211ed6bca9f6aa8646e364
danielcobo <danielcobo@users.noreply.github.com>
2016-12-06 10:40:51 +0100

    Remove section indents

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@
 Imports
 =======
 */
-	//Official Firebase SDK
-	var firebase = require('firebase');
+//Official Firebase SDK
+var firebase = require('firebase');
 
 /*
 Helper functions
@@ -34,193 +34,193 @@ var handleError = function handleError(error, fail){
 Init
 ====
 */
-	//----- Helper functions -----
-		/*
-		Initialise app and return app object
-		@param - config [object]
-		*/
-		var initApp = function initApp(config){
-			var dbURL = config.databaseURL
+//----- Helper functions -----
+/*
+Initialise app and return app object
+@param - config [object]
+*/
+var initApp = function initApp(config){
+	var dbURL = config.databaseURL
 
-			if (!app.list[dbURL]){
-				var ref = firebase.initializeApp(config, dbURL);
-				//NOTE: There is no 'default' app
-				app.list[dbURL] = {ref: ref};
-			}
-			
-			return app.list[dbURL];
-		};
-		
-		/*
-		Return app object of initialised app
-		@param - dbURL [string]
-		*/
-		var getApp = function getApp(dbURL){
-			var ref = app.list[dbURL];
-			if (!ref){
-				var err = ['App '',
-							dbURL,
-							'' not initialised.'
-							].join('');
-				throw new Error(err);
-			}
-			else{
-				return ref;
-			}
-		};
+	if (!app.list[dbURL]){
+		var ref = firebase.initializeApp(config, dbURL);
+		//NOTE: There is no 'default' app
+		app.list[dbURL] = {ref: ref};
+	}
 	
-	//----- App -----
-		/*
-		Return database reference object
-		@param - dbURL [string] or config [object]
-		*/
-		var app = function app(param){
-			if (typeof param === 'string'){
-				//dbURL === param
-				return getApp(param).ref;
-			}
-			else{			
-				//param === config
-				return initApp(param).ref;
-			}
-		};
-	
-	//-----All app objects-----
-		app.list = {};
+	return app.list[dbURL];
+};
+
+/*
+Return app object of initialised app
+@param - dbURL [string]
+*/
+var getApp = function getApp(dbURL){
+	var ref = app.list[dbURL];
+	if (!ref){
+		var err = ['App '',
+					dbURL,
+					'' not initialised.'
+					].join('');
+		throw new Error(err);
+	}
+	else{
+		return ref;
+	}
+};
+
+//----- App -----
+/*
+Return database reference object
+@param - dbURL [string] or config [object]
+*/
+var app = function app(param){
+	if (typeof param === 'string'){
+		//dbURL === param
+		return getApp(param).ref;
+	}
+	else{			
+		//param === config
+		return initApp(param).ref;
+	}
+};
+
+//-----All app objects-----
+app.list = {};
 
 /*
 Users
 =====
 */
-	app.addUser = function addUser(dbURL, user, callback, fail){
-		app(dbURL)
-			.auth()
-			.createUserWithEmailAndPassword(user.email, user.password)
-			.then(
-				function(){ handleCallback(callback); },
-				function(error){ handleError(error, fail); }
-				);
-	};
-	
-	//For internal use (not to handle login/logout)
-	app.getUser = function getUser(dbURL, callback){
-		var user = app(dbURL).auth().currentUser;
-		if (user){
-			handleCallback(user, callback);
-		}
-		else{
-			handleError('No user', fail);
-		}
-	};
-	
-	//To handle login/logout
-	app.watchUser = function watchUser(dbURL, callback){
-		app(dbURL)
-			.auth()
-			.onAuthStateChanged(function (user){
-				if (user){
-					handleCallback(user, callback);
-				}
-				else{
-					handleError('No user', fail);
-				}
-			});
-	};
-		
-	app.updateUser = function updateUser(dbURL, profile, callback, fail){
-		app.getUser(dbURL, function(user){
-			user.updateProfile(profile)
-				.then(
-					function(){ handleCallback(callback); },
-					function(error){ handleError(error, fail); }
-					);
-		});
-	};
-	
-	app.updateUserEmail = function updateUserEmail(dbURL, email, callback, fail){
-		app.getUser(dbURL, function(user){
-			user.updateEmail(email)
-				.then(
-					function(){ handleCallback(callback); },
-					function(error){ handleError(error, fail); }
-					);
-		});
-	};
-	
-	app.updateUserPassword = function updateUserPassword(dbURL, password, callback, fail){
-		app.getUser(dbURL, function(user){
-			user.updatePassword(password)
-				.then(
-					function(){ handleCallback(callback); },
-					function(error){ handleError(error, fail); }
-					);
-		});
-	};
+app.addUser = function addUser(dbURL, user, callback, fail){
+	app(dbURL)
+		.auth()
+		.createUserWithEmailAndPassword(user.email, user.password)
+		.then(
+			function(){ handleCallback(callback); },
+			function(error){ handleError(error, fail); }
+			);
+};
 
-	app.userEmailVerification = function userEmailVerification(dbURL, callback, fail){
-		app.getUser(dbURL, function(user){
-			user.sendEmailVerification()
-				.then(
-					function(){ handleCallback(callback); },
-					function(error){ handleError(error, fail); }
-					);
+//For internal use (not to handle login/logout)
+app.getUser = function getUser(dbURL, callback){
+	var user = app(dbURL).auth().currentUser;
+	if (user){
+		handleCallback(user, callback);
+	}
+	else{
+		handleError('No user', fail);
+	}
+};
+
+//To handle login/logout
+app.watchUser = function watchUser(dbURL, callback){
+	app(dbURL)
+		.auth()
+		.onAuthStateChanged(function (user){
+			if (user){
+				handleCallback(user, callback);
+			}
+			else{
+				handleError('No user', fail);
+			}
 		});
-	};
+};
 	
-	app.userEmailResetPassword = function userEmailResetPassword(dbURL, email, callback, fail){
-		app.getUser(dbURL, function(user){
-			app(dbURL)
-				.auth()
-				.sendPasswordResetEmail(email)
-				.then(
-					function(){ handleCallback(callback); },
-					function(error){ handleError(error, fail); }
-					);
-		});
-	};
-	
-	app.removeUser = function removeUser(dbURL, callback, fail){
-		app.getUser(dbURL, function (user){
-			user.delete()
-				.then(
-					function(){ handleCallback(callback); },
-					function(error){ handleError(error, fail); }
-					);
-		});
-	};
-	
-	app.loginUser = function loginUser(dbURL, user, callback, fail){
-		app(dbURL)
-			.auth()
-			.signInWithEmailAndPassword(user.email, user.password)
+app.updateUser = function updateUser(dbURL, profile, callback, fail){
+	app.getUser(dbURL, function(user){
+		user.updateProfile(profile)
 			.then(
 				function(){ handleCallback(callback); },
 				function(error){ handleError(error, fail); }
 				);
-	};
-	
-	app.logoutUser = function logoutUser(dbURL, callback, fail){
-		app(dbURL)
-			.auth()
-			.signOut()
+	});
+};
+
+app.updateUserEmail = function updateUserEmail(dbURL, email, callback, fail){
+	app.getUser(dbURL, function(user){
+		user.updateEmail(email)
 			.then(
 				function(){ handleCallback(callback); },
 				function(error){ handleError(error, fail); }
 				);
-	};
-	
-	app.reauthUser = function reauthUser(dbURL, credentials, callback, fail){
-		app.getUser(dbURL, function(user){
-			user.reauthenticate(credentials)
-				.then(
-					function(){ handleCallback(callback); },
-					function(error){ handleError(error, fail); }
-					);
-		});
-	};
+	});
+};
+
+app.updateUserPassword = function updateUserPassword(dbURL, password, callback, fail){
+	app.getUser(dbURL, function(user){
+		user.updatePassword(password)
+			.then(
+				function(){ handleCallback(callback); },
+				function(error){ handleError(error, fail); }
+				);
+	});
+};
+
+app.userEmailVerification = function userEmailVerification(dbURL, callback, fail){
+	app.getUser(dbURL, function(user){
+		user.sendEmailVerification()
+			.then(
+				function(){ handleCallback(callback); },
+				function(error){ handleError(error, fail); }
+				);
+	});
+};
+
+app.userEmailResetPassword = function userEmailResetPassword(dbURL, email, callback, fail){
+	app.getUser(dbURL, function(user){
+		app(dbURL)
+			.auth()
+			.sendPasswordResetEmail(email)
+			.then(
+				function(){ handleCallback(callback); },
+				function(error){ handleError(error, fail); }
+				);
+	});
+};
+
+app.removeUser = function removeUser(dbURL, callback, fail){
+	app.getUser(dbURL, function (user){
+		user.delete()
+			.then(
+				function(){ handleCallback(callback); },
+				function(error){ handleError(error, fail); }
+				);
+	});
+};
+
+app.loginUser = function loginUser(dbURL, user, callback, fail){
+	app(dbURL)
+		.auth()
+		.signInWithEmailAndPassword(user.email, user.password)
+		.then(
+			function(){ handleCallback(callback); },
+			function(error){ handleError(error, fail); }
+			);
+};
+
+app.logoutUser = function logoutUser(dbURL, callback, fail){
+	app(dbURL)
+		.auth()
+		.signOut()
+		.then(
+			function(){ handleCallback(callback); },
+			function(error){ handleError(error, fail); }
+			);
+};
+
+app.reauthUser = function reauthUser(dbURL, credentials, callback, fail){
+	app.getUser(dbURL, function(user){
+		user.reauthenticate(credentials)
+			.then(
+				function(){ handleCallback(callback); },
+				function(error){ handleError(error, fail); }
+				);
+	});
+};
 
 /*
 Exports
 =======
 */
-	module.exports = app;
+module.exports = app;
